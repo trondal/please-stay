4921a831e220b5440f1e5f6e96a0b09bf4a10e9f
Trond Albinussen <trond.albinussen@blinq.no>
2023-03-10 08:59:21 +0100

    Failed build, but close

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,81 @@
-import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json' assert { type: 'json' };
+//import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: pkg.main,
-    format: 'module',
-    sourcemap: true,
-    strict: false
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import packageJson from './package.json' assert { type: 'json' };
+import { getFolders } from './scripts/buildUtils.js';
+
+const plugins = [
+  peerDepsExternal(),
+  resolve(),
+  replace({
+    __IS_DEV__: process.env.NODE_ENV === 'development'
+  }),
+  commonjs(),
+  typescript({
+    tsconfig: './tsconfig.json',
+    useTsconfigDeclarationDir: true
+  }),
+  terser()
+];
+
+const subfolderPlugins = (folderName) => [
+  ...plugins,
+  generatePackageJson({
+    baseContents: {
+      name: `${packageJson.name}/${folderName}`,
+      private: true,
+      main: '../cjs/index.js', // --> points to cjs format entry point of whole library
+      module: './index.js', // --> points to esm format entry point of individual component
+      types: './index.d.ts' // --> points to types definition file of individual component
+    }
+  })
+];
+
+const folderBuilds = getFolders('./src').map((folder) => {
+  return {
+    input: `src/${folder}/index.ts`,
+    output: {
+      file: `dist/${folder}/index.js`,
+      sourcemap: true,
+      exports: 'named',
+      format: 'esm'
+    },
+    plugins: subfolderPlugins(folder),
+    external: ['react', 'react-dom']
+  };
+});
+
+export default [
+  {
+    input: ['src/index.ts'],
+    output: [
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    plugins,
+    external: ['react', 'react-dom']
   },
-  plugins: [typescript()],
-  external: ['react']
-};
+  ...folderBuilds,
+  {
+    input: ['src/index.ts'],
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    plugins,
+    external: ['react', 'react-dom']
+  }
+];
